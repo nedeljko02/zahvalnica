e5c80d650be89bb831bdbf735a31ab4a311adf45
Luka Nedeljković <117765558+nedeljko02@users.noreply.github.com>
2026-04-17 16:39:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-// Kada učita stranicu, pripremi QR kod
+
 document.addEventListener('DOMContentLoaded', function() {
     
     const googlePhotosLink = "https://photos.google.com/share/AF1QipOqgP14vbc_MCBZjcEvsDjSVeaPikrcUzQlXY_Hex9Nkv9JF0a_HksJRHf6QpvOJw?key=MVVmano4dW9SQTdJbGh4S1c1UERBeTZGenBMbXFn";
     const qrCodeImg = document.getElementById('qrCode');
     
-    // Kada se klikne na QR kod, otvori Google Photos
     qrCodeImg.addEventListener('click', function() {
         window.open(googlePhotosLink, '_blank');
     });
-    
-    // Dodaj tooltip
+  
     qrCodeImg.title = "Klikni ili skeniraj da otvoriš galeriju!";
 });
